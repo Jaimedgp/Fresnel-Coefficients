78904dd209c3eda8bdea9d1d9ec983a8db3a0bce
Jaime <jaime.diez.gp@gmail.com>
2017-11-01 18:21:34 +0100

    Calc and Plot R coeff for n>n' with both n and n' reals

--- CalcFrsnlCoeff.py
+++ CalcFrsnlCoeff.py
@@ -1,7 +1,7 @@
 import numpy as np
-from math import pi
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
+################################################################################
 
 class FresnelCoefficients:
 
@@ -9,8 +9,31 @@ class FresnelCoefficients:
 
 		self.n = n
 		self.ni = ni
-		self.phi = np.linspace(0,pi/2,200)
+		self.phi = np.linspace(0,np.pi/2,200)
 
 	def realCoeff(self):
 
+		phii = [np.arcsin(np.sin(phi)*(self.n/self.ni)) for phi in self.phi ]
+
+		rParl = []
+		rOrt = []
+
+		for i in range(len(phii)):
+			rOrt.append(-np.sin(self.phi[i]-phii[i]) / 
+				                                 np.sin(self.phi[i]+phii[i]))
+
+			rParl.append(-np.tan(self.phi[i]-phii[i]) /
+			                                     np.tan(self.phi[i]+phii[i]))
+
+		plt.axhline(0, color='black').set_dashes([1,1,1,1])
+		plt.plot(self.phi, rParl, label="$r_{||}$")
+		plt.plot(self.phi, rOrt, label="$r_{\perp}$")
 		
+		plt.xticks([0,np.pi/2])
+		plt.xlim([0,np.pi/2])
+		
+		plt.yticks([-1,0,1])
+		plt.ylim([-1,1])
+
+		plt.legend()
+		plt.show()
